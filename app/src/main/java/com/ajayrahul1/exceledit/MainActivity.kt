3e70420e2c8d35b930da44550f50956ffb975750
AjayRahul1 <p.ajayrahul02@gmail.com>
2022-11-11 20:03:55 +0530

    Adapter for MainActivity.kt set

--- app/src/main/java/com/ajayrahul1/exceledit/MainActivity.kt
+++ app/src/main/java/com/ajayrahul1/exceledit/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ajayrahul1.exceledit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.ajayrahul1.exceledit.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.hide()
+        val items = listOf("Customers.xlsx", "Finance.xlsx", "Components.xlsx")
+        val adapterItems = ArrayAdapter(this, R.layout.dropdown_item, items)
+        binding.dropdownExcelOptions.setAdapter(adapterItems)
     }
 }
