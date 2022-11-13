f728c5b647122de4a32bc8520a72d22096b62f59
AjayRahul1 <p.ajayrahul02@gmail.com>
2022-11-13 10:34:51 +0530

    Alignments of buttons changed and new elements added
    Create button is seprated from remaining buttons.\nDisplay excel button commented out.\nNew Edit Text Added for create excel name input and it is invisible by default

--- app/src/main/java/com/ajayrahul1/exceledit/MainActivity.kt
+++ app/src/main/java/com/ajayrahul1/exceledit/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ajayrahul1.exceledit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import android.widget.Toast.makeText
@@ -16,16 +17,48 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar?.hide()    // Hides the action bar
 
-        val items = listOf("Customers.xlsx", "Finance.xlsx", "Components.xlsx")
-        val adapterItems = ArrayAdapter(this, R.layout.dropdown_item, items)
-        binding.DropDownExcelFiles.setAdapter(adapterItems)
+        val items = mutableListOf("Customers.xlsx", "Finance.xlsx", "Components.xlsx")
+        setItemsToDropDownMenu(items)
 
-        binding.DropDownExcelFiles.setOnItemClickListener { parent, view, position, id ->
+        binding.DropDownExcelFiles.setOnItemClickListener { parent, _, position, _ ->
             makeText(
                 applicationContext,
-                parent.getItemAtPosition(position).toString() + " file deleted",
+                "${parent.getItemAtPosition(position)} file selected",
                 Toast.LENGTH_SHORT
             ).show()
+        }
+
+        binding.btnCreateExcel.setOnClickListener{
+            createExcel(items)
+        }
+
+        binding.btnDeleteExcel.setOnClickListener {
+            deleteExcel(items)
+        }
+    }
+
+    private fun createExcel(items: MutableList<String>) {
+        binding.createExcelInputOutline.visibility =  View.VISIBLE
+        binding.etCreateExcelInput.visibility = View.VISIBLE
+    }
+
+    private fun setItemsToDropDownMenu(items: MutableList<String>) {
+        val adapterItems = ArrayAdapter(this, R.layout.dropdown_item, items)
+        binding.DropDownExcelFiles.setAdapter(adapterItems)
+    }
+
+    private fun deleteExcel(items: MutableList<String>) {
+        makeText(
+            applicationContext,
+            "${binding.DropDownExcelFiles.text} file deleted",
+            Toast.LENGTH_SHORT
+        ).show()
+        items.remove(binding.DropDownExcelFiles.text.toString())
+        if (items.isEmpty())
+            binding.DropDownExcelFiles.setText("No files left")
+        else if (!items.contains(binding.DropDownExcelFiles.text.toString())) {
+            binding.DropDownExcelFiles.setText("Select another one")
+            setItemsToDropDownMenu(items)
         }
     }
 }
