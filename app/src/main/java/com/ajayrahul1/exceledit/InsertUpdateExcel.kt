cbfa10ed33d063af9895c391ceec872364b99e9e
AjayRahul1 <p.ajayrahul02@gmail.com>
2022-11-16 23:59:51 +0530

    Files that are working added

--- app/src/main/java/com/ajayrahul1/exceledit/InsertUpdateExcel.kt
+++ app/src/main/java/com/ajayrahul1/exceledit/InsertUpdateExcel.kt
@@ -1,10 +1,12 @@
 package com.ajayrahul1.exceledit
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
 import com.ajayrahul1.exceledit.databinding.ActivityInsertUpdateExcelBinding
-import com.ajayrahul1.exceledit.databinding.ActivityMainBinding
 
 class InsertUpdateExcel : AppCompatActivity() {
     private lateinit var modifyExcelBind: ActivityInsertUpdateExcelBinding
@@ -13,6 +15,20 @@ class InsertUpdateExcel : AppCompatActivity() {
         modifyExcelBind = ActivityInsertUpdateExcelBinding.inflate(layoutInflater)
         setContentView(modifyExcelBind.root)
 
+        modifyExcelBind.noOfColumnsCount.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(view, keyCode)
+        }
+    }
 
+    // Function that closes keyboard when clicked ENTER KEY
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
